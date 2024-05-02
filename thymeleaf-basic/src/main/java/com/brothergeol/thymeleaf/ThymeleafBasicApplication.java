f7368d0d80522414f8762bd294838744e3cf394f
LeeHyungGeol <rjf1138@naver.com>
2024-05-02 17:10:34 +0900

    feat(타임리프:-기본-기능): 기본 객체들

--- thymeleaf-basic/src/main/java/com/brothergeol/thymeleaf/ThymeleafBasicApplication.java
+++ thymeleaf-basic/src/main/java/com/brothergeol/thymeleaf/ThymeleafBasicApplication.java
@@ -1,13 +1,19 @@
 package com.brothergeol.thymeleaf;
 
+import com.brothergeol.thymeleaf.basic.BasicController.HelloBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ThymeleafBasicApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ThymeleafBasicApplication.class, args);
+		ConfigurableApplicationContext ac = SpringApplication.run(ThymeleafBasicApplication.class, args);
+//		HelloBean bean = ac.getBean(HelloBean.class);
+//		for (String beanDefinitionName : ac.getBeanDefinitionNames()) {
+//			System.out.println("beanDefinitionName = " + beanDefinitionName);
+//		}
 	}
 
 }
