c642cb542d70ec4ce11a587934607d3a69e844f6
LeeHyungGeol <rjf1138@naver.com>
2024-05-14 16:50:45 +0900

    feat(API-예외-처리): @ExceptionHandler

---
+++ exception/src/main/java/com/brothergeol/exception/exceptionhandler/ErrorResult.java
@@ -0,0 +1,1 @@
+package com.brothergeol.exception.exceptionhandler;import lombok.AllArgsConstructor;import lombok.Data;@Data@AllArgsConstructorpublic class ErrorResult {    private String code;    private String message;}
