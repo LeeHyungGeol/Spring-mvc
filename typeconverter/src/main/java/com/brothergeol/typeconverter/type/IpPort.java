3a2323db860e026aa23143b1cf5f61dc8133ffeb
LeeHyungGeol <rjf1138@naver.com>
2024-05-16 16:38:49 +0900

    feat(스프링-타입-컨버터): 타입 컨버터 - Converter

---
+++ typeconverter/src/main/java/com/brothergeol/typeconverter/type/IpPort.java
@@ -0,0 +1,1 @@
+package com.brothergeol.typeconverter.type;import lombok.EqualsAndHashCode;import lombok.Getter;@Getter@EqualsAndHashCodepublic class IpPort {    private String ip;    private Integer port;    public IpPort(String ip, Integer port) {        this.ip = ip;        this.port = port;    }}
